ac3c07abe258bdc51f077cd3ecfe28c5b44c71a5
De General <aubynsamuel05@gmail.com>
2025-02-18 22:51:58 +0000

    added support for expo notification service

--- server.js
+++ server.js
@@ -71,6 +71,47 @@ async function getUserDetails(userId) {
   }
 }
 
+async function sendNotificationWithENS(
+  recipientsToken,
+  title,
+  body,
+  roomId,
+  recipientsUserId,
+  sendersUserId,
+  profileUrl
+) {
+  const message = {
+    to: recipientsToken,
+    title,
+    body: body.length < 100 ? body : body.substring(0, 100) + "...",
+    data: {
+      recipientsUserId,
+      sendersUserId,
+      roomId,
+      profileUrl,
+    },
+    sound: "default",
+    priority: "high",
+    channelId: "fcm_fallback_notification_channel",
+  };
+
+  try {
+    const response = await fetch("https://exp.host/--/api/v2/push/send", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Accept-encoding": "gzip, deflate",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(message),
+    });
+    return await response.json();
+  } catch (error) {
+    console.error("Failed to send notification:", error);
+    return null;
+  }
+}
+
 /**
  * Send a notification using Firebase Cloud Messaging (FCM) via the Admin SDK.
  *
@@ -196,15 +237,27 @@ app.post("/api/reply", async (req, res) => {
     );
 
     if (recipient.deviceToken) {
-      await sendNotification(
-        recipient.deviceToken,
-        sender.username,
-        replyText,
-        roomId,
-        recipientsUserId,
-        sendersUserId,
-        sender.profileUrl
-      );
+      if (recipient.deviceToken?.includes("ExponentPushToken")) {
+        await sendNotificationWithENS(
+          recipient.deviceToken,
+          sender.username,
+          replyText,
+          roomId,
+          recipientsUserId,
+          sendersUserId,
+          sender.profileUrl
+        );
+      } else {
+        await sendNotification(
+          recipient.deviceToken,
+          sender.username,
+          replyText,
+          roomId,
+          recipientsUserId,
+          sendersUserId,
+          sender.profileUrl
+        );
+      }
     }
 
     res.status(200).json({ success: true, message: "Reply sent successfully" });
