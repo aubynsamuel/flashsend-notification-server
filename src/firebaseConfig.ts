25ebc6d38c5350696adbd3fb1f2ffb3e46c9534e
De General <aubynsamuel05@gmail.com>
2025-05-20 06:35:39 +0000

    Initial

---
+++ src/firebaseConfig.ts
@@ -0,0 +1,17 @@
+interface IFirebase {
+  apiKey: string | undefined;
+  authDomain: string | undefined;
+  projectId: string | undefined;
+  storageBucket: string | undefined;
+  messagingSenderId: string | undefined;
+  appId: string | undefined;
+}
+
+export const firebaseConfig: IFirebase = {
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId,
+};
